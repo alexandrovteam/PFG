b1837b5182d09d22883d93b6dd9384ed0b3b5411
Artem Tarasov <artem.tarasov@embl.de>
2016-07-13 14:29:57 +0200

    fix build.sh for os x

--- conda-build/build.sh
+++ conda-build/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if [ `uname` == 'Darwin' ]; then
+  # since Apple doesn't care enough to provide its users with OpenMP-supporting compiler by default, so do we
+  export NO_OPENMP=1
+fi
+
 make -j4 && cp PFG $PREFIX/bin/PFG
