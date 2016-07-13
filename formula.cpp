3684d6eb62ac1252c3f82dbaaef95edb64fcffdd
Artem Tarasov <artem.tarasov@embl.de>
2016-07-13 11:03:33 +0200

    added .clang-format, fixed gcc warnings

--- formula.cpp
+++ formula.cpp
@@ -14,240 +14,240 @@
 
 using namespace std;
 
-double *elmass(vector<char*> compositions, int size){
-	double *_elmass;
-	_elmass = new double [size];
-	double temp;
-	for(int i = 0; i < size; i++){
-		temp = elementmass(compositions[i]);
-		_elmass[i] = temp;
-	}
-	return _elmass;
+double *elmass(vector<char *> compositions, int size) {
+  double *_elmass;
+  _elmass = new double[size];
+  double temp;
+  for (int i = 0; i < size; i++) {
+    temp = elementmass(compositions[i]);
+    _elmass[i] = temp;
+  }
+  return _elmass;
 }
 
-double delta(double measuredMass, double countedMass, char units[]="ppm"){
-	// Calculate error between measured Mass and counted Mass in specified units.
-	if(strcmp(units,"ppm")==0)
-		return (measuredMass - countedMass) / countedMass*1000000;
-	else if(strcmp(units,"Da")==0)
-		return (measuredMass - countedMass);
-	else if(strcmp(units,"%")==0)
-		return (measuredMass - countedMass) / countedMass*100;
-	else printf("Unknown units for delta!");
-	    return false;
+double delta(double measuredMass, double countedMass,
+             const char *units = "ppm") {
+  // Calculate error between measured Mass and counted Mass in specified units.
+  if (strcmp(units, "ppm") == 0)
+    return (measuredMass - countedMass) / countedMass * 1000000;
+  else if (strcmp(units, "Da") == 0)
+    return (measuredMass - countedMass);
+  else if (strcmp(units, "%") == 0)
+    return (measuredMass - countedMass) / countedMass * 100;
+  else
+    printf("Unknown units for delta!");
+  return false;
 }
 
-
-
-void PFG(result *p_result, int elcount, int minimum[], int maximum[], double masses[], int *current, double pre_mass,double loMass, double hiMass)
-	//call the template metaprogramming
+void PFG(result *p_result, int elcount, int minimum[], int maximum[],
+         double masses[], int *current, double pre_mass, double loMass,
+         double hiMass)
+// call the template metaprogramming
 {
-	int k = 0;
-	switch (elcount)
-	{
-		case 1:  formula_generator<1>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 2:  formula_generator<2>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 3:  formula_generator<3>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 4:  formula_generator<4>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 5:  formula_generator<5>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 6:  formula_generator<6>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 7:  formula_generator<7>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 8:  formula_generator<8>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 9:  formula_generator<9>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 10: formula_generator<10>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 11: formula_generator<11>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 12: formula_generator<12>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 13: formula_generator<13>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 14: formula_generator<14>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-		case 15: formula_generator<15>::EXEC(p_result, elcount, minimum, maximum, masses, current, pre_mass, loMass, hiMass, k); break;
-	default:
-		break;
-	}
+  int k = 0;
+  switch (elcount) {
+    case 1:
+      formula_generator<1>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 2:
+      formula_generator<2>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 3:
+      formula_generator<3>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 4:
+      formula_generator<4>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 5:
+      formula_generator<5>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 6:
+      formula_generator<6>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 7:
+      formula_generator<7>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 8:
+      formula_generator<8>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 9:
+      formula_generator<9>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                 current, pre_mass, loMass, hiMass, k);
+      break;
+    case 10:
+      formula_generator<10>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                  current, pre_mass, loMass, hiMass, k);
+      break;
+    case 11:
+      formula_generator<11>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                  current, pre_mass, loMass, hiMass, k);
+      break;
+    case 12:
+      formula_generator<12>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                  current, pre_mass, loMass, hiMass, k);
+      break;
+    case 13:
+      formula_generator<13>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                  current, pre_mass, loMass, hiMass, k);
+      break;
+    case 14:
+      formula_generator<14>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                  current, pre_mass, loMass, hiMass, k);
+      break;
+    case 15:
+      formula_generator<15>::EXEC(p_result, elcount, minimum, maximum, masses,
+                                  current, pre_mass, loMass, hiMass, k);
+      break;
+    default:
+      break;
+  }
 }
 
-void calculation(double currentmass, vector<char*> compositions, int mincount[], int maxcount[], vector<char*> rules, float tolerance, int charge, char *agentformula, int agentcharge, char units[])
-{
-	double _mz = mz(currentmass, 0, charge, agentformula, agentcharge);
-	int elcount = compositions.size();
-	double finalmass=_mz;
-	double loMass,hiMass;
-	if (strcmp(units,"ppm") == 0 && charge == 0){
-		loMass = finalmass - (finalmass/1000000) * tolerance;
-		hiMass = finalmass + (finalmass/1000000) * tolerance;
-	}
-	else if (charge != 0 && strcmp(units,"ppm") == 0)
-	{
-		loMass = finalmass - abs(charge) * (finalmass/1000000) * tolerance;
-		hiMass = finalmass + abs(charge) * (finalmass/1000000) * tolerance;
-	}
-	else if (charge!=0 && strcmp(units,"Da")==0)
-	{
-		loMass = finalmass - abs(charge) * tolerance;
-		hiMass = finalmass + abs(charge) * tolerance;
-	}
-	else 
-	{
-		loMass = finalmass - tolerance;
-		hiMass = finalmass + tolerance;
-	}
+void calculation(double currentmass, vector<char *> compositions,
+                 int mincount[], int maxcount[], vector<char *> rules,
+                 float tolerance, int charge, const char *agentformula,
+                 int agentcharge, const char *units) {
+  double _mz = mz(currentmass, 0, charge, agentformula, agentcharge);
+  int elcount = compositions.size();
+  double finalmass = _mz;
+  double loMass, hiMass;
+  if (strcmp(units, "ppm") == 0 && charge == 0) {
+    loMass = finalmass - (finalmass / 1000000) * tolerance;
+    hiMass = finalmass + (finalmass / 1000000) * tolerance;
+  } else if (charge != 0 && strcmp(units, "ppm") == 0) {
+    loMass = finalmass - abs(charge) * (finalmass / 1000000) * tolerance;
+    hiMass = finalmass + abs(charge) * (finalmass / 1000000) * tolerance;
+  } else if (charge != 0 && strcmp(units, "Da") == 0) {
+    loMass = finalmass - abs(charge) * tolerance;
+    hiMass = finalmass + abs(charge) * tolerance;
+  } else {
+    loMass = finalmass - tolerance;
+    hiMass = finalmass + tolerance;
+  }
 
-	double *_elmass = elmass(compositions, elcount);
+  double *_elmass = elmass(compositions, elcount);
 
-	for(int i = 0; i < elcount; i++)
-	{
-		if (strcmp(compositions[i], "C") == 0)
-		{
-			if (_mz < 500)
-			{
-				maxcount[i] = min(maxcount[i], 40);
-			}
-			else if (_mz < 1000)
-			{
-				maxcount[i] = min(maxcount[i], 80);
-			}
-			else if (_mz < 2000)
-			{
-				maxcount[i] = min(maxcount[i], 160);
-			}
-			else maxcount[i] = min(maxcount[i], 180);
-		}
-		else if (strcmp(compositions[i], "H") == 0)
-		{
-			if (_mz < 500)
-			{
-				maxcount[i] = min(maxcount[i], 80);
-			}
-			else if (_mz < 1000)
-			{
-				maxcount[i] = min(maxcount[i], 130);
-			}
-			else if (_mz < 2000)
-			{
-				maxcount[i] = min(maxcount[i], 250);
-			}
-			else maxcount[i] = min(maxcount[i], 300);
-		}
-		else if (strcmp(compositions[i], "O") == 0)
-		{
-			if (_mz < 500)
-			{
-				maxcount[i] = min(maxcount[i], 20);
-			}
-			else if (_mz < 1000)
-			{
-				maxcount[i] = min(maxcount[i], 30);
-			}
-			else if (_mz < 2000)
-			{
-				maxcount[i] = min(maxcount[i], 70);
-			}
-			else maxcount[i] = min(maxcount[i], 90);
-		}
-		else if (strcmp(compositions[i], "N") == 0)
-		{
-			if (_mz < 500)
-			{
-				maxcount[i] = min(maxcount[i], 20);
-			}
-			else if (_mz < 1000)
-			{
-				maxcount[i] = min(maxcount[i], 30);
-			}
-			else if (_mz < 2000)
-			{
-				maxcount[i] = min(maxcount[i], 40);
-			}
-			else maxcount[i] = min(maxcount[i], 60);
-		}
-		maxcount[i] = min(maxcount[i], int(hiMass / _elmass[i]));
+  for (int i = 0; i < elcount; i++) {
+    if (strcmp(compositions[i], "C") == 0) {
+      if (_mz < 500) {
+        maxcount[i] = min(maxcount[i], 40);
+      } else if (_mz < 1000) {
+        maxcount[i] = min(maxcount[i], 80);
+      } else if (_mz < 2000) {
+        maxcount[i] = min(maxcount[i], 160);
+      } else
+        maxcount[i] = min(maxcount[i], 180);
+    } else if (strcmp(compositions[i], "H") == 0) {
+      if (_mz < 500) {
+        maxcount[i] = min(maxcount[i], 80);
+      } else if (_mz < 1000) {
+        maxcount[i] = min(maxcount[i], 130);
+      } else if (_mz < 2000) {
+        maxcount[i] = min(maxcount[i], 250);
+      } else
+        maxcount[i] = min(maxcount[i], 300);
+    } else if (strcmp(compositions[i], "O") == 0) {
+      if (_mz < 500) {
+        maxcount[i] = min(maxcount[i], 20);
+      } else if (_mz < 1000) {
+        maxcount[i] = min(maxcount[i], 30);
+      } else if (_mz < 2000) {
+        maxcount[i] = min(maxcount[i], 70);
+      } else
+        maxcount[i] = min(maxcount[i], 90);
+    } else if (strcmp(compositions[i], "N") == 0) {
+      if (_mz < 500) {
+        maxcount[i] = min(maxcount[i], 20);
+      } else if (_mz < 1000) {
+        maxcount[i] = min(maxcount[i], 30);
+      } else if (_mz < 2000) {
+        maxcount[i] = min(maxcount[i], 40);
+      } else
+        maxcount[i] = min(maxcount[i], 60);
+    }
+    maxcount[i] = min(maxcount[i], int(hiMass / _elmass[i]));
+  }
 
-	}
+  result *p_result = new result[1000000];
+  p_result->data = new int[1000000 * elcount];
+  p_result->len = 0;
+  p_result->mass = new double[1000000];
+  int *current = new int[elcount];
 
-	result *p_result = new result [1000000];
-	p_result->data = new int [1000000 * elcount];
-	p_result->len = 0;
-	p_result->mass = new double [1000000];
-	int *current = new int [elcount];
+  for (int i = 0; i < elcount; i++) {
+    current[i] = mincount[i];
+  }
+  double pre_mass = 0.0;
+  PFG(p_result, elcount, mincount, maxcount, _elmass, current, pre_mass, loMass,
+      hiMass);
 
-	for (int i = 0; i < elcount; i++)
-	{
-		current[i] = mincount[i];
-	}
-	double pre_mass = 0.0;
-	PFG(p_result, elcount, mincount, maxcount, _elmass, current, pre_mass, loMass, hiMass);
+  FILE *fpt;
+  fpt = fopen("result.txt", "w");
+  fprintf(fpt, "formula\t mass\t mz\t error\t rdbe\n");
+  char *temp = new char[100];
+  char *s = new char[100];
 
-	FILE *fpt; 
-	fpt=fopen("result.txt","w");
-	fprintf(fpt,"formula\t mass\t mz\t error\t rdbe\n");
-	char *temp = new char[100];
-	char *s = new char[100];
+  // start = clock();
+  for (int i = 0; i < p_result->len; i++) {
+    temp[0] = '\0';
+    s[0] = '\0';
+    int countC = 0;
+    int countH = 0;
+    int countO = 0;
+    int countN = 0;
+    int countP = 0;
+    int countS = 0;
+    double rdbevalue;
+    vector<int> count;
+    for (int j = 0; j < elcount; j++) {
+      if (strcmp(compositions[j], "C") == 0) {
+        countC = p_result->data[i * elcount + j];
+      } else if (strcmp(compositions[j], "H") == 0) {
+        countH = p_result->data[i * elcount + j];
+      } else if (strcmp(compositions[j], "O") == 0) {
+        countO = p_result->data[i * elcount + j];
+      } else if (strcmp(compositions[j], "N") == 0) {
+        countN = p_result->data[i * elcount + j];
+      } else if (strcmp(compositions[j], "P") == 0) {
+        countP = p_result->data[i * elcount + j];
+      } else if (strcmp(compositions[j], "S") == 0) {
+        countS = p_result->data[i * elcount + j];
+      }
 
-	//start = clock();
-	for(int i = 0; i < p_result->len; i++)
-	{
-		temp[0]='\0';
-		s[0]='\0';
-		int countC = 0;
-		int countH = 0;
-		int countO = 0;
-		int countN = 0;
-		int countP = 0;
-		int countS = 0;
-		double rdbevalue;
-		vector<int> count;
-		for(int j = 0; j < elcount; j++)
-		{
-			if (strcmp(compositions[j], "C") == 0)
-			{
-				countC = p_result->data[i*elcount + j];
-			}
-			else if (strcmp(compositions[j], "H") == 0)
-			{
-				countH = p_result->data[i*elcount + j];
-			}
-			else if (strcmp(compositions[j], "O") == 0)
-			{
-				countO = p_result->data[i*elcount + j];
-			}
-			else if (strcmp(compositions[j], "N") == 0)
-			{
-				countN = p_result->data[i*elcount + j];
-			}
-			else if (strcmp(compositions[j], "P") == 0)
-			{
-				countP = p_result->data[i*elcount + j];
-			}
-			else if (strcmp(compositions[j], "S") == 0)
-			{
-				countS = p_result->data[i*elcount + j];
-			}			
+      if (p_result->data[i * elcount + j] != 0) {
+        sprintf(s, "%s%d", compositions[j], p_result->data[i * elcount + j]);
+        strncat(temp, s, strlen(s));
+      }
 
-			if(p_result->data[i*elcount + j] !=0)
-			{
-				sprintf(s, "%s%d",compositions[j], p_result->data[i*elcount + j]);
-				strncat(temp, s, strlen(s));
-			}
+      count.push_back(p_result->data[i * elcount + j]);
+    }
+    rdbevalue = rdbe(compositions, count);
 
-			count.push_back(p_result->data[i*elcount + j]);
-		}
-		rdbevalue = rdbe(compositions, count);
-	
-		int t=frules(temp, countC, countH, countO, countN, countP, countS, rdbevalue, rules);
-		if(t == 1)
-		{
-			double mass = p_result->mass[i];
-			double _mass = mass;
-			double fmz = mz(_mass, charge, 0, agentformula, agentcharge);
-			double error = delta(currentmass, fmz, units);
-			fprintf(fpt, "%s\t %f\t %f\t %f\t %f\n", temp, mass, fmz, error, rdbevalue);
-		}
-	}
-	fclose(fpt);
-	delete []temp;
-	delete []s;
-	delete []_elmass;
-	delete []p_result->mass;
-	delete []p_result->data;
-	delete []p_result;
-	delete []current;
+    int t = frules(temp, countC, countH, countO, countN, countP, countS,
+                   rdbevalue, rules);
+    if (t == 1) {
+      double mass = p_result->mass[i];
+      double _mass = mass;
+      double fmz = mz(_mass, charge, 0, agentformula, agentcharge);
+      double error = delta(currentmass, fmz, units);
+      fprintf(fpt, "%s\t %f\t %f\t %f\t %f\n", temp, mass, fmz, error,
+              rdbevalue);
+    }
+  }
+  fclose(fpt);
+  delete[] temp;
+  delete[] s;
+  delete[] _elmass;
+  delete[] p_result->mass;
+  delete[] p_result->data;
+  delete[] p_result;
+  delete[] current;
 }
--- formula.h
+++ formula.h
@@ -1,10 +1,10 @@
-#ifndef __FORMULA_H   
-#define __FORMULA_H  
-#define SUBSLEN 10             
-#define EBUFLEN 128         
-#define BUFLEN 1024  
-#define ELECTRON_MASS  0.00054857990924
-#define min(a,b) a<b?a:b
+#ifndef __FORMULA_H
+#define __FORMULA_H
+#define SUBSLEN 10
+#define EBUFLEN 128
+#define BUFLEN 1024
+#define ELECTRON_MASS 0.00054857990924
+#define min(a, b) a < b ? a : b
 
 #include <string>
 #include <map>
@@ -12,55 +12,61 @@
 #include <vector>
 using namespace std;
 
-typedef struct{
-	char *name;
-	char *symbol;
-	int atomicNumber;
-	int isotopecount;
-	int massnumber[10];
-	double isotope[10];
-	double abundance[10];
-	int valence;
-}ellist;
+typedef struct {
+  char *name;
+  char *symbol;
+  int atomicNumber;
+  int isotopecount;
+  int massnumber[10];
+  double isotope[10];
+  double abundance[10];
+  int valence;
+} ellist;
 
-typedef struct{
-	char **comp;
-	int *atomcount;
-	int elcount;
-}composition;
+typedef struct {
+  char **comp;
+  int *atomcount;
+  int elcount;
+} composition;
 
-typedef struct{
-	int *data;
-	int len;
-	double *mass;
-}result;
+typedef struct {
+  int *data;
+  int len;
+  double *mass;
+} result;
 
-typedef struct{
-	char *_formula;
-	double *_mass;
-	double *_mz;
-	double error;
-	double errorDa;
-	double rdbe;
-}output;
+typedef struct {
+  char *_formula;
+  double *_mass;
+  double *_mz;
+  double error;
+  double errorDa;
+  double rdbe;
+} output;
 
-//elements
-double elementmass(char *string);
-double rdbe(vector<char*> compositions, vector<int> count);
-double formulaMass(vector<char*> compositions, vector<int> count);
+// elements
+double elementmass(const char *string);
+double rdbe(vector<char *> compositions, vector<int> count);
+double formulaMass(vector<char *> compositions, vector<int> count);
 
-//compound
-double mz(double currentmass,int finalcharge, int currentcharge, char *agentformula, int agentcharge);
-static double GLHC[2]={0.2, 3};
-static double GLNOPSC[4]={2, 1.2, 0.32, 0.65};
-static double GLRDBE[2]={0, 40};
-int frules(char *string, int countC, int countH, int countO, int countN, int countP, int countS, double rdbevalue, 
-		   vector<char*> rules);
+// compound
+double mz(double currentmass, int finalcharge, int currentcharge,
+          const char *agentformula, int agentcharge);
+static double GLHC[2] = { 0.2, 3 };
+static double GLNOPSC[4] = { 2, 1.2, 0.32, 0.65 };
+static double GLRDBE[2] = { 0, 40 };
+int frules(char *string, int countC, int countH, int countO, int countN,
+           int countP, int countS, double rdbevalue, vector<char *> rules);
 
-//formula
-double *elmass(vector<char*> compositions, int size);
-double delta(double measuredMass, double countedMass, char units[]);
-void PFG(result *p_result, int elcount, int minimum[], int maximum[], double masses[], int *current, double pre_mass,double loMass, double hiMass);
-void calculation(double currentmass, vector<char*> compositions, int mincount[], int maxcount[], vector<char*> rules, float tolerance=1,  
-		  int charge = 0, char *agentformula = "H", int agentcharge=1, char units[]="ppm");
+// formula
+double *elmass(vector<char *> compositions, int size);
+double delta(double measuredMass, double countedMass, const char *units);
+void PFG(result *p_result, int elcount, int minimum[], int maximum[],
+         double masses[], int *current, double pre_mass, double loMass,
+         double hiMass);
+void calculation(double currentmass, vector<char *> compositions,
+                 int mincount[], int maxcount[], vector<char *> rules,
+                 float tolerance = 1, int charge = 0,
+                 const char *agentformula = "H", int agentcharge = 1,
+                 const char *units = "ppm");
 #endif
